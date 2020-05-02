59604bc97a606ca23d64258869f3f2682857898c
ADEM OLGUNER <ademolguner@gmail.com>
2020-05-02 15:35:07 +0300

    create project one

---
+++ SompoTech/SompoTech.Nuget/DataConvert/ObjectConvertFormatManager.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace SompoTech.Nuget.DataConvert
+{
+    public class ObjectConvertFormatManager : IObjectConvertFormat
+    {
+        public T JsonToObject<T>(string jsonString) where T : class, new()
+        {
+            var objectData = JsonConvert.DeserializeObject<T>(jsonString);
+            return objectData;
+        }
+
+        public string ObjectToJson<T>(T entityObject) where T : class, new()
+        {
+            var jsonString = JsonConvert.SerializeObject(entityObject);
+            return jsonString;
+        }
+
+        public T ParseObjectDataArray<T>(byte[] rawBytes) where T : class, new()
+        {
+            var stringData = Encoding.UTF8.GetString(rawBytes);
+            return JsonToObject<T>(stringData);
+        }
+    }
+}
